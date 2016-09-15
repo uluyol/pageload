2f87dc4660e301fb872a4dd79b7086d093694b24
Muhammed Uluyol <uluyol0@gmail.com>
2016-09-14 21:25:10 -0400

    auto_ec2: ignore ssh warnings

--- auto_ec2/setup_docker.bash
+++ auto_ec2/setup_docker.bash
@@ -2,11 +2,17 @@
 
 set -e
 
+SSH() {
+	ssh \
+		-o StrictHostKeyChecking=no \
+		"$@"
+}
+
 nodes=("$@")
 
 for n in "${nodes[@]}"; do
 	echo setup $n...
-	ssh "$n" "\
+	SSH "$n" "\
 		sudo apt-get update && \
 		sudo apt-get install tmux && \
 		curl -fsSL https://get.docker.com/ | sudo bash"
