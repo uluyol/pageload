16552675baaf146afd92cdaba0387a74ae422980
Muhammed Uluyol <uluyol0@gmail.com>
2016-09-14 21:25:44 -0400

    auto_ec2: do not worry about existing data, add timeouts to pulling

--- auto_ec2/pull_sharded_page_load.bash
+++ auto_ec2/pull_sharded_page_load.bash
@@ -11,10 +11,10 @@ SSH() {
 destdir=$1; shift
 nodes=("$@")
 
-if [[ -e $destdir ]]; then
-	echo error: $destdir already exists >&2
-	exit 1
-fi
+#if [[ -e $destdir ]]; then
+#	echo error: $destdir already exists >&2
+#	exit 1
+#fi
 
 mkdir -p "$destdir"
 
@@ -23,21 +23,16 @@ while true; do
 		if [[ $(SSH "$n" "ps ax" | grep run_shard.bash | wc -l) -lt 2 ]]; then
 			echo possible error on $n, incorrect number of run_shard.bash instances
 		fi
-		to_move=()
 		for dev in $(SSH "$n" "ls -1 shard_dl"); do
 			iters=($(SSH "$n" "ls -1 shard_dl/$dev" | sort -g))
-				# leave the last two for now, they may be incomplete
-				for ((i=0; i < ${#iters[@]}-2; i++)); do
-					to_move+=("$dev/${iters[i]}")
-				done
+			# leave the last two for now, they may be incomplete
+			for ((i=0; i < ${#iters[@]}-2; i++)); do
+				to_move="$dev/${iters[i]}"
+				echo get $n $to_move
+				(cd "$destdir" && SSH "$n" "cd shard_dl && tar cz $to_move" | timeout 300 tar -xzf -)
+				timeout 90 SSH "$n" "cd shard_dl && rm -rf $to_move"
+			done
 		done
-		if (( ${#to_move[@]} < 1 )); then
-			echo nothing to get for $n
-			continue
-		fi
-		printf "get $n %s\n" "${to_move[@]}"
-		(cd "$destdir" && SSH "$n" "cd shard_dl && tar cz ${to_move[@]}" | tar -xzf -)
-		SSH "$n" "cd shard_dl && rm -rf ${to_move[@]}"
 	done
 	echo sleep
 	sleep 60
