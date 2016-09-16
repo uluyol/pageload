dfd66b7c3c8d26762e67e060430dfd02a74ecf65
Muhammed Uluyol <uluyol0@gmail.com>
2016-09-16 15:06:20 -0400

    cmd/internal: add new versions of get_{online,offline}_dep_list.bash that use extracted data

---
+++ cmd/internal/extracted_get_offline_dep_list.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# assume that PATH has already been set by a previous script
+# as this is for internal use only
+
+set -e
+
+extracted=$(cat)
+
+index_urls=$(jq -r .indexRedirectChain[] <<<"$extracted")
+index_last_url=$(tail -n1 <<<"$index_urls")
+online=$(jq -r .indexBody <<<"$extracted" | _internal_get_online_deps.py "$index_last_url")
+to_remove=$(cat <(echo "$index_urls") <(echo "$online") | sort | uniq)
+
+_setops_ignore_types.py setdiff \
+	<(jq -r '.resources[] | .url + " " + .contentType' <<<"$extracted" | sort) \
+	<(echo "$to_remove" | sort)
