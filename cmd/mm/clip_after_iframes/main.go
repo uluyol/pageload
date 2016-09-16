93b7c833659a101e331d5b563b8dd7f485d6e994
Muhammed Uluyol <uluyol0@gmail.com>
2016-09-16 15:05:16 -0400

    cmd/mm/clip_after_iframe: new cmd to clip deps that are children of iframes

---
+++ cmd/mm/clip_after_iframes/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/uluyol/pageload/cmd/mm/internal"
+)
+
+type Result struct {
+	Resources          []ResourceMeta `json:"resources"`
+	IndexRedirectChain []string       `json:"indexRedirectChain"`
+	IndexBody          string         `json:"indexBody"`
+}
+
+type ResourceMeta struct {
+	Bytes       int64  `json:"bytes"`
+	URL         string `json:"url"`
+	ContentType string `json:"contentType"`
+	Referer     string `json:"referer"`
+}
+
+type ResourceMetaSet map[ResourceMeta]struct{}
+
+func NewResourceMetaSet() ResourceMetaSet {
+	return ResourceMetaSet(make(map[ResourceMeta]struct{}))
+}
+
+func (set ResourceMetaSet) Add(e ResourceMeta) {
+	set[e] = struct{}{}
+}
+
+func (set ResourceMetaSet) All() []ResourceMeta {
+	var members []ResourceMeta
+	for e, _ := range set {
+		members = append(members, e)
+	}
+	return members
+}
+
+func addRecursive(set ResourceMetaSet, refResMap map[string][]ResourceMeta, ref string) {
+	for _, rm := range refResMap[ref] {
+		set.Add(rm)
+		if strings.Contains(rm.ContentType, "html") {
+			continue
+		}
+		for ref, _ := range refResMap {
+			if internal.CleanURL(ref) == internal.CleanURL(rm.URL) {
+				addRecursive(set, refResMap, ref)
+			}
+		}
+	}
+}
+
+type byURL []ResourceMeta
+
+func (s byURL) Len() int           { return len(s) }
+func (s byURL) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s byURL) Less(i, j int) bool { return s[i].URL < s[j].URL }
+
+func main() {
+	flag.Parse()
+
+	log.SetPrefix("clip_after_iframes: ")
+	log.SetFlags(0)
+
+	var result Result
+	dec := json.NewDecoder(os.Stdin)
+	if err := dec.Decode(&result); err != nil {
+		log.Fatalf("unable to decode result: %v", err)
+	}
+
+	refererResourceMap := make(map[string][]ResourceMeta)
+	for _, res := range result.Resources {
+		children := refererResourceMap[res.Referer]
+		children = append(children, res)
+		refererResourceMap[res.Referer] = children
+	}
+
+	set := NewResourceMetaSet()
+	indexURL := result.IndexRedirectChain[len(result.IndexRedirectChain)-1]
+
+	for ref, _ := range refererResourceMap {
+		if internal.CleanURL(ref) == internal.CleanURL(indexURL) {
+			// is root
+			addRecursive(set, refererResourceMap, ref)
+		}
+	}
+
+	result.Resources = set.All()
+	sort.Sort(byURL(result.Resources))
+	enc := json.NewEncoder(os.Stdout)
+	enc.Encode(&result)
+}
