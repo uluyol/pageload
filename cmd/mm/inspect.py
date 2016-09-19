0c13995816fc217045451256da2bce4479ae7e14
Muhammed Uluyol <uluyol0@gmail.com>
2016-09-18 21:26:02 -0400

    cmd/mm: rename get_index.py to inspect, really is just a debug tool

--- cmd/mm/inspect.py
+++ cmd/mm/inspect.py
@@ -9,18 +9,17 @@ from google.protobuf import text_format
 import http_record_pb2 as pb
 
 def main():
-	site = sys.argv[1]
-	savedir = sys.argv[2]
+	savedir = sys.argv[1]
 
 	saves = os.listdir(savedir)
 
 	for s in saves:
-		print(s)
 		req_resp = pb.RequestResponse()
 		with open(os.path.join(savedir, s), "rb") as f:
 			encoded = f.read()
 			req_resp.ParseFromString(encoded)
-		print(req_resp.request.first_line)
+		print(req_resp.request.first_line.decode("utf-8"))
+		print(req_resp.request.header)
 
 if __name__ == "__main__":
 	main()
