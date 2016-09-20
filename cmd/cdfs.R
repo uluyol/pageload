5a9ef07fc64e07a82903f702930f1842931344c2
Muhammed Uluyol <uluyol0@gmail.com>
2016-09-19 23:58:25 -0400

    cmd: use dark lines

--- cmd/cdfs.R
+++ cmd/cdfs.R
@@ -37,7 +37,7 @@ options(warn = -1)
 		scale_x_continuous(breaks=xaxis.breaks, limits=c(NA, tail(xaxis.breaks, 1))) +
 		xlab(xaxis.label) +
 		ylab("CDF") +
-		scale_color_brewer(palette="Set1") +
+		scale_color_brewer(palette="Dark2") +
 		theme_bw() +
 		theme(legend.title=element_blank())
 options(warn = 0)
